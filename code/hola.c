70775163701ef5bc106f37a05cbac96d98b973d2
JoseAPulido <124211963+JoseAPulido@users.noreply.github.com>
2023-02-16 14:45:51 -0800

    Update hola.c

--- code/hola.c
+++ code/hola.c
@@ -6,3 +6,4 @@ int main() {
     return 0;
 }
 
+![](https://images.unsplash.com/photo-1533450718592-29d45635f0a9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8anBnfGVufDB8fDB8fA%3D%3D&w=1000&q=80)
